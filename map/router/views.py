c36ac03a7b67453656db270b2b1d95ef1655aee7
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2025-06-24 11:02:52 +0545

    validate request data for get route API

--- map/router/views.py
+++ map/router/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import RouteRequestSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['POST'])
 def get_route(request):
+    """
+    Get route between two places (start and end)
+    """
+
+    serializer = RouteRequestSerializer(data=request.data)
+    if not serializer.is_valid():
+        return Response(
+            {"error": "Invalid input", "details": serializer.errors}, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    
+
+
     return HttpResponse("Welcome, ! Here you canget the route from third party API")
