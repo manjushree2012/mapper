6174a7d38608478a670c9ee37de90bdf41aeae1b
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2025-06-24 11:02:35 +0545

    install rest framework as dependency

--- map/map/settings.py
+++ map/map/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # edited from here
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
