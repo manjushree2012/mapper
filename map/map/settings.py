da0502f51eeb3176f3e3054819d5b0934bfffceb
Roshan Manjushree Adhikari <manjushreeroshan@gmail.com>
2025-06-24 11:50:23 +0545

    set ORS API KEY in settings for config

--- map/map/settings.py
+++ map/map/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ORS_API_KEY = config('ORS_API_KEY', default='your-ors-api-key-here')
